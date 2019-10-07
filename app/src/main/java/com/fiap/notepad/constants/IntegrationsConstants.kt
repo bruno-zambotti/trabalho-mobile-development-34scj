367938328ab56dafa5ff3f91a2d78ef9673699aa
Bruno Delphino Zambotti <52378865+bruno-zambotti@users.noreply.github.com>
2019-10-07 19:49:35 -0300

    Ajuste na url para teste local

--- app/src/main/java/com/fiap/notepad/constants/IntegrationsConstants.kt
+++ app/src/main/java/com/fiap/notepad/constants/IntegrationsConstants.kt
@@ -1,5 +1,5 @@
 package com.fiap.notepad.constants
 
 object IntegrationsConstants {
-    const val URL_NOTEPAD_API = "http://localhost:3000/notes/"
+    const val URL_NOTEPAD_API = "http://10.0.2.2:3000/notes/"
 }
